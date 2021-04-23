02fd2a2cf2315c3264e20c898a07d8db8ab14978
Imtiaz ali <imtiaz.ali@m3tech.com.pk>
2021-04-23 14:34:21 +0500

    initial

--- src/main/java/com/example/registration/RegistrationServiceApplication.java
+++ src/main/java/com/example/registration/RegistrationServiceApplication.java
@@ -2,14 +2,22 @@ package com.example.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class RegistrationServiceApplication {
+public class RegistrationServiceApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RegistrationServiceApplication.class, args);
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RegistrationServiceApplication.class);
 	}
 
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(RegistrationServiceApplication.class, args);
+	}
 }
--- src/main/java/com/example/registration/controller/RegistrationController.java
+++ src/main/java/com/example/registration/controller/RegistrationController.java
@@ -1,13 +1,28 @@
 package com.example.registration.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class RegistrationController {
 
     @GetMapping("/")
+
     public String test(){
         return "This is registration service";
     }
+
+    @GetMapping("/register")
+    public String register(){
+        return "This is registration method";
+    }
+    @GetMapping(value = "/circuit")
+
+    public String hello() throws InterruptedException {
+        Thread.sleep(3000);
+        return "Welcome Hystrix";
+    }
+
 }
